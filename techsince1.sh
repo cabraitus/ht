067058f1e877adc03cb8e8d2256f64f8d7663675
cabraitus <44943077+cabraitus@users.noreply.github.com>
2018-11-11 13:05:33 +0000

    Add files via upload

---
+++ techsince1.sh
@@ -0,0 +1,85 @@
+#!/bin/bash
+echo $red 
+ echo ""----------------------------------------------------------------------------- ""
+echo " This Script is written by Tech Since Yt Channel. enjoy"
+echo "---------------------------------------------------------------------------------"
+
+echo "007"
+echo "007"
+echo "007"
+echo "."
+
+echo  "#####################################"
+echo   "  SUBSCRIBE    TECH SINCE      "               
+ echo   " FOR TECH &  ETHICAL HACKING VIDEOS "                                                                    
+echo   "####################################"
+
+echo "007"
+echo "007"
+echo "007"
+
+echo "."
+
+apt update -y                      #update in progress
+apt install python -y           #installation......
+apt install python2 -y
+apt install ruby -y 
+apt install git -y
+
+echo  "#####################################"
+echo   "  SUBSCRIBE  TECH SINCE   "               
+ echo   " FOR TECH &  ETHICAL HACKING VIDEOS "                                                                    
+echo   "####################################"
+
+apt install php -y 
+apt install perl -y
+apt install nmap -y
+
+echo  "#####################################"
+echo   "  SUBSCRIBE    TECH SINCE      "               
+ echo   " FOR TECH &  ETHICAL HACKING VIDEOS "                                                                    
+echo   "####################################"
+
+apt install bash -y
+apt install clang -y
+apt install jq -y
+apt install macchanger -y
+
+echo  "#####################################"
+echo   "  SUBSCRIBE    TECH SINCE      "               
+ echo   " FOR TECH &  ETHICAL HACKING VIDEOS "                                                                    
+echo   "####################################"
+
+apt install nano -y
+apt install curl -y
+apt install tar -y
+apt install zip -y
+
+echo  "#####################################"
+echo   "  SUBSCRIBE    TECH SINCE      "               
+ echo   " FOR TECH &  ETHICAL HACKING VIDEOS "                                                                    
+echo   "####################################"
+
+apt install unzip -y
+apt install tor -y 
+apt install sudo -y
+apt install wget -y
+
+echo  "#####################################"
+echo   "  SUBSCRIBE    TECH SINCE      "               
+ echo   " FOR TECH &  ETHICAL HACKING VIDEOS "                                                                    
+echo   "####################################"
+
+apt install wcalc -y 
+apt install openssl
+apt install bmon -y
+
+echo  "#####################################"
+echo   "  SUBSCRIBE    TECH SINCE      "               
+ echo   " FOR TECH &  ETHICAL HACKING VIDEOS "                                                                    
+echo   "####################################"
+
+echo " If You Connected To Internet Then  "
+echo " YOUR PACKAGES ARE INSTALLED SUCCESSFULLY "
+
+exit
